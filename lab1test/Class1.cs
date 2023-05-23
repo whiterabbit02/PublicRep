d90d6cbfee12425b7149a5121a5897eead508ae7
Mashenkin Oleg <71937799+whiterabbit02@users.noreply.github.com>
2023-05-24 03:10:34 +0700

    Add files via upload

---
+++ lab1test/Class1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Lab1test
+{
+    class Program
+    {
+
+        public static int[] input()
+        {
+            string st;
+            int a;
+            Console.WriteLine("Введите количество товаров: ");
+            st = Console.ReadLine();
+            a = Convert.ToInt32(st);
+
+            int[] tovar = new int[a];
+
+            Console.WriteLine("Введите стоимость товаров: ");
+
+            for (int i = 0; i < a; i++)
+            {
+                st = Console.ReadLine();
+                tovar[i] = Convert.ToInt32(st);
+            }
+
+            return tovar;
+        }
+
+        public static int kolVishSr(int[] tovar)
+        {
+            int summ, k, sr, sch;
+            summ = 0;
+            sch = 0;
+            k = tovar.Length;
+
+            for (int i = 0; i < k; i++)
+            {
+                summ = summ + tovar[i];
+                sch++;
+            }
+
+            sr = summ / sch;
+            sch = 0;
+
+            for (int i = 0; i < k; i++)
+            {
+                if (tovar[i] > sr)
+                {
+                    sch++;
+                }
+            }
+
+            return sch;
+        }
+
+        public static int[] sort(int[] tovar)
+        {
+            string st;
+            int[] index = new int[1];
+            int dell, schet = 0, m, p;
+
+            Console.WriteLine("Товар какой стоимостью удалить?");
+            st = Console.ReadLine();
+            dell = Convert.ToInt32(st);
+
+            for (int i = 0; i < tovar.Length; i++)
+            {
+                if (tovar[i] == dell)
+                {
+                    schet++;
+                }
+            }
+
+            int[] tovarIsp = new int[tovar.Length - schet];
+
+            m = tovar.Length;
+            p = 0;
+
+            do
+            {
+                if (tovar[p] == dell)
+                {
+                    for (int j = p; j < m - 1; j++) tovar[j] = tovar[j + 1];
+                    m--;
+                }
+                else
+                {
+                    p++;
+                }
+            }
+            while (p <= m);
+
+            for (int i = 0; i < tovarIsp.Length; i++)
+            {
+                tovarIsp[i] = tovar[i];
+            }
+
+            return tovarIsp;
+        }
+
+        public static void output(int[] a)
+        {
+            Console.WriteLine("Новый массив оставшихся товаров: ");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] tov = input();
+            Console.WriteLine("Количество товаров, стоимость которых выше средней стоимости: " + kolVishSr(tov));
+            int[] ispTov = sort(tov);
+            output(ispTov);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1test/Class2.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Lab1test;
+
+namespace Lab1test.Tests
+{
+    [TestFixture]
+    public class Class2
+    {
+        [Test]
+        public void Test_input_WithPositiveInteger_ReturnsArrayWithGivenSize()
+        {
+            int[] expected = new int[5];
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("5\n1\n2\n3\n4\n5\n"));
+
+            result = Program.input();
+
+            Assert.AreEqual(expected.Length, result.Length);
+        }
+
+        [Test]
+        public void Test_kolVishSr_WithArray_ReturnsCountOfElementsGreaterThanAverage()
+        {
+            int[] tovar = { 1, 2, 3, 4, 5 };
+            int expected = 2;
+
+            int result = Program.kolVishSr(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_sort_WithArrayAndExistingValue_ReturnsArrayWithRemovedValue()
+        {
+            int[] tovar = { 1, 2, 3, 4, 5 };
+            int[] expected = { 1, 2, 4, 5 };
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("3\n"));
+
+            result = Program.sort(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_output_WithArray_WritesArrayElementsToConsole()
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            string expectedOutput = "Новый массив оставшихся товаров:\n1\n2\n3\n4\n5\n";
+
+            // Capture console output
+            var consoleOutput = new System.IO.StringWriter();
+            Console.SetOut(consoleOutput);
+
+            Program.output(a);
+
+            Assert.AreEqual(expectedOutput, consoleOutput.ToString());
+        }
+
+        [Test]
+        public void Test_input_WithZeroItems_ReturnsEmptyArray()
+        {
+            int[] expected = new int[0];
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("0\n"));
+
+            result = Program.input();
+
+            Assert.AreEqual(expected.Length, result.Length);
+        }
+
+        [Test]
+        public void Test_kolVishSr_WithEmptyArray_ReturnsZero()
+        {
+            int[] tovar = new int[0];
+            int expected = 0;
+
+            int result = Program.kolVishSr(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_sort_WithArrayAndNonExistingValue_ReturnsSameArray()
+        {
+            int[] tovar = { 1, 2, 3, 4, 5 };
+            int[] expected = { 1, 2, 3, 4, 5 };
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("10\n"));
+
+            result = Program.sort(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_output_WithEmptyArray_WritesNoOutputToConsole()
+        {
+            int[] a = new int[0];
+            string expectedOutput = "";
+
+            // Capture console output
+            var consoleOutput = new System.IO.StringWriter();
+            Console.SetOut(consoleOutput);
+
+            Program.output(a);
+
+            Assert.AreEqual(expectedOutput, consoleOutput.ToString());
+        }
+
+        [Test]
+        public void Test_input_WithNegativeInteger_ReturnsArrayWithGivenSize()
+        {
+            int[] expected = new int[3];
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("3\n1\n2\n3\n"));
+
+            result = Program.input();
+
+            Assert.AreEqual(expected.Length, result.Length);
+        }
+
+        [Test]
+        public void Test_kolVishSr_WithArrayWithEqualElements_ReturnsZero()
+        {
+            int[] tovar = { 1, 1, 1, 1, 1 };
+            int expected = 0;
+
+            int result = Program.kolVishSr(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_sort_WithEmptyArray_ReturnsEmptyArray()
+        {
+            int[] tovar = new int[0];
+            int[] expected = new int[0];
+            int[] result;
+
+            // Mock user input
+            Console.SetIn(new System.IO.StringReader("10\n"));
+
+            result = Program.sort(tovar);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Test_output_WithArrayWithNegativeValues_WritesArrayElementsToConsole()
+        {
+            int[] a = { -1, -2, -3, -4, -5 };
+            string expectedOutput = "Новый массив оставшихся товаров:\n-1\n-2\n-3\n-4\n-5\n";
+
+            // Capture console output
+            var consoleOutput = new System.IO.StringWriter();
+            Console.SetOut(consoleOutput);
+
+            Program.output(a);
+
+            Assert.AreEqual(expectedOutput, consoleOutput.ToString());
+        }
+    }
+}
